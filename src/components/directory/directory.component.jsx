1898fded95944ea3a355f6882937fb74d6437828
pimpaka stewart <35515307+pimpaka@users.noreply.github.com>
2020-04-20 14:45:40 +0700

    update menu-items to navigate withRouter HOC

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -12,7 +12,7 @@ class Directory extends React.Component {
           title: 'hats',
           imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
           id: 1,
-          linkUrl: 'shop/hats'
+          linkUrl: 'hats'
         },
         {
           title: 'jackets',
@@ -50,8 +50,8 @@ class Directory extends React.Component {
     return (
       <div className='directory-menu'>
         {
-          this.state.sections.map(({id, title, size, imageUrl}) => (
-            <MenuItem key={id} title={title} size={size} imageUrl={imageUrl} />
+          this.state.sections.map(({id, ...otheSectionState}) => (
+            <MenuItem key={id} {...otheSectionState} />
           ))
         }
       </div>
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import './menu-item.styles.scss'
+import { withRouter } from 'react-router-dom';
 
-const MenuItem = ({ title, size, imageUrl }) => (
-  <div className={`${size} menu-item`}>
+const MenuItem = ({ title, size, imageUrl, history, match, linkUrl }) => (
+  <div 
+    className={`${size} menu-item`} 
+    onClick={() => history.push(`${match.url}${linkUrl}`)}
+  >
     <div
       className='background-image'
       style={{
@@ -16,4 +20,4 @@ const MenuItem = ({ title, size, imageUrl }) => (
   </div>
 )
 
-export default MenuItem;
+export default withRouter(MenuItem);
