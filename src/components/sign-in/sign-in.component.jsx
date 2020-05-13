86b8108d8e1b45db0cacbe8e5d56d1b2726a1a7a
pimpaka stewart <35515307+pimpaka@users.noreply.github.com>
2020-05-13 10:18:52 +0700

    feat: sign-in sign-up components and create custom button

---
+++ src/components/sign-in/sign-in.component.jsx
@@ -0,0 +1,59 @@
+import React, { Component } from "react";
+import "./sign-in.styles.scss";
+import FormInput from "../form-input/form-input.component";
+import CustomButton from "../custom-button/custom-button.component";
+
+class SignIn extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: "",
+    };
+  }
+
+  handleSubmit = (event) => {
+    this.setState({
+      email: "",
+      password: "",
+    });
+  };
+
+  handleChange = (event) => {
+    const { value, name } = event.target;
+
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  render() {
+    return (
+      <div className="sign-in">
+        <h2 className="title">I already have an account</h2>
+        <span>Sign in with your email and password</span>
+
+        <form onSubmit={this.handleSubmit}>
+          <FormInput
+            name="email"
+            value={this.state.email}
+            handleChange={this.handleChange}
+            label="Email"
+            required
+          />
+          <FormInput
+            name="password"
+            type="password"
+            label="Password"
+            value={this.state.password}
+            handleChange={this.handleChange}
+            required
+          />
+          <CustomButton type="submit">Sign in</CustomButton>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SignIn;
